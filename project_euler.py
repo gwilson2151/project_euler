2911fb35b2c5ad3423513eb66c8bc0ad92762a03
Gavin Wilson <gavin.wilson@visioncritical.com>
2016-04-06 14:07:15 -0700

    tabs to spaces

--- project_euler.py
+++ project_euler.py
@@ -1,5 +1,5 @@
 import project_euler_library as pe_lib
 
 def n1():
-	target = 999
-	return pe_lib.sum_divisible_by(target, 3) + pe_lib.sum_divisible_by(target, 5) - pe_lib.sum_divisible_by(target, 15)
+    target = 999
+    return pe_lib.sum_divisible_by(target, 3) + pe_lib.sum_divisible_by(target, 5) - pe_lib.sum_divisible_by(target, 15)
--- project_euler_library.py
+++ project_euler_library.py
@@ -1,6 +1,6 @@
 def sum_divisible_by(target, n):
-	p = target // n
-	return n * sum_series_from_1(p)
+    p = target // n
+    return n * sum_series_from_1(p)
 
 def sum_series_from_1(p):
-	return p * (p + 1) // 2
+    return p * (p + 1) // 2
